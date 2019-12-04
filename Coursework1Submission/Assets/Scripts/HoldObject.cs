280cc0838f981e0c16f909e00111ca62b65584e2
ie2990 <eliai@lsbu.ac.uk>
2019-12-04 16:27:09 +0000

    tut3

---
+++ Coursework1Submission/Assets/Scripts/HoldObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldObject : MonoBehaviour
+{
+
+            public Transform Destination;
+
+    private void OnMouseDown()
+    {
+        //removing gravity
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<BoxCollider>().enabled = false;
+
+        this.transform.position = Destination.position;
+        this.transform.parent = GameObject.Find("Destination").transform;
+    }
+
+    private void OnMouseUp()
+    {
+        this.transform.parent = null;
+
+        GetComponent<BoxCollider>().enabled = true;
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+}
+

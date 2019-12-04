b4596e78b969f2574641599a6ca3ab877d0bd4a2
ie2990 <eliai@lsbu.ac.uk>
2019-12-04 18:07:02 +0000

    Final tut doneee

---
+++ Coursework1Submission/Assets/Scripts/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    public float startingTime =10f;
+    public float currentTime = 0f;
+
+    [SerializeField] Text timerText;
+
+    private void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    private void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+
+        timerText.text = currentTime.ToString("0.0");
+
+        if (currentTime <= 0.9)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+    }
+
+}
